340a0d7ab926ba2903559e4704521857fa39d8bd
alexander <alexis24ever@gmail.com>
2022-12-27 12:21:25 +0300

    create start point

--- lesson7_step9.py
+++ lesson7_step9.py
@@ -1,39 +1,42 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support import expected_conditions as ec
 import time
 # import os
 import math
 
 
-def calc(x):
-  return str(math.log(abs(12*math.sin(int(x)))))
+def calc(x_pr: str):
+    return str(math.log(abs(12 * math.sin(int(x_pr)))))
 
 
-try: 
-    link1 = " http://suninjuly.github.io/explicit_wait2.html"
-    browser = webdriver.Chrome()
-    browser.get(link1)
+if __name__ == "__main__":
+    try:
+        link1 = "http://suninjuly.github.io/explicit_wait2.html"
+        # s = Service('/usr/local/share/chromedriver')
+        browser1 = webdriver.Chrome()   # service=s)
+        browser1.get(link1)
 
-    # Ваш код, который заполняет обязательные поля
-    
-    WebDriverWait(browser, 12).until(EC.text_to_be_present_in_element((By.ID, "price"), "$100"))
+        # Ваш код, который заполняет обязательные поля
 
-    button = browser.find_element(By.ID, "book")
-    button.click()
+        WebDriverWait(browser1, 12).until(ec.text_to_be_present_in_element((By.ID, "price"), "$100"))
 
-    x = browser.find_element(By.ID, "input_value").text
-    answer = browser.find_element(By.ID, "answer")
-    browser.execute_script("return arguments[0].scrollIntoView(true);", answer)
-    answer.send_keys(str(calc(x)))
+        button = browser1.find_element(By.ID, "book")
+        button.click()
 
-    button = browser.find_element(By.ID, "solve")
-    browser.execute_script("return arguments[0].scrollIntoView(true);", button)
-    button.click()
+        x = browser1.find_element(By.ID, "input_value").text
+        answer = browser1.find_element(By.ID, "answer")
+        browser1.execute_script("return arguments[0].scrollIntoView(true);", answer)
+        answer.send_keys(str(calc(x)))
 
-finally:
-    # ожидание чтобы визуально оценить результаты прохождения скрипта
-    time.sleep(10)
-    # закрываем браузер после всех манипуляций
-    browser.quit()
+        button = browser1.find_element(By.ID, "solve")
+        browser1.execute_script("return arguments[0].scrollIntoView(true);", button)
+        button.click()
+
+    finally:
+        # ожидание чтобы визуально оценить результаты прохождения скрипта
+        time.sleep(10)
+        # закрываем браузер после всех манипуляций
+        browser1.quit()
