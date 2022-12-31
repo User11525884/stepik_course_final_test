0f8b406128ebe8ef74ad943eec8fc9804d7f9ea9
User11525884 <64400740+User11525884@users.noreply.github.com>
2022-12-31 15:34:18 +0300

    Create locators.py

---
+++ locators.py
@@ -0,0 +1,5 @@
+from selenium.webdriver.common.by import By
+
+
+class MainPageLocators():
+    LOGIN_LINK = (By.CSS_SELECTOR, "#registration_link")  # теперь каждый селектор — это пара: как искать и что искать. 
